cda317a086ed3cd09cc2541d75322b88c3db234e
Astar <bahouidi99@gmail.com>
2024-04-21 19:32:32 +0100

    Lists in React
    Usually using an index should be avoided though, because it comes with
    the same rendering performance issues from above. In addition, it can
    cause actual bugs in the UI⁵¹ whenever the order of items got changed
    (e.g. re-ordering, appending or removing items). However, as last
    resort, if the list does not change its order in any way, using the
    index is fine.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-const title = "React";
+import { list } from "./constants/mock_data.ts";
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}!</h1>
+      <h1>My hacker stories</h1>
       <label htmlFor="search">Search: </label>&nbsp;
       <input id="search" type="text" />
+      <hr />
+      <ul>
+        {list.map((item) => (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/constants/mock_data.ts
@@ -0,0 +1,34 @@
+export const list = [
+  {
+    title: "React",
+    url: "http://reactjs.org/",
+    author: "Jordan Walkie",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "http://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: "Vue",
+    url: "http://vuejs.org/",
+    author: "Earl McCormack",
+    num_comments: 13,
+    points: 7,
+    objectID: 2,
+  },
+  {
+    title: "Angular",
+    url: "http://angular.io/",
+    author: "Premus Prepyak",
+    num_comments: 9,
+    points: 8,
+    objectID: 3,
+  },
+];

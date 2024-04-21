bc68d65c0882a7c7ff52ea0db3957d04a0369bdc
Astar <bahouidi99@gmail.com>
2024-04-21 22:31:04 +0100

    Lifting state
    	Always manage state at a component level where every component
    that's interested in it is one that either manages the state
    (using information directly from state, e.g. App component) or a
    component below the state managing component
    (using information from props, e.g. List or Search components).
    
    	If a component below needs to update the state (e.g. Search),
    pass a callback handler down to it which allows this particular
    component to update the state above in the parent component.
    If a component below needs to use the state (e.g. displaying it),
    pass it down as props.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
+import { useState } from "react";
+
 import List from "./components/List.tsx";
 import Search from "./components/Search.tsx";
 import { list } from "./constants/mock_data.ts";
 
 function App() {
   const stories = list;
+  const [searchTerm, setSearchTerm] = useState("");
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(`From App component: ${event.target.value}`)
-  }
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((item) =>
+    item.title.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
 
   return (
     <div>
       <h1>My hacker stories</h1>
-      <Search onSearch={handleSearch}/>
+      <Search onSearch={handleSearch} />
       <hr />
-      <List stories={stories} />
+      <List stories={searchedStories} />
     </div>
   );
 }
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,23 +1,12 @@
-import { useState } from "react";
-
 type SearchProps = {
   onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 const Search: React.FC<SearchProps> = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
-    onSearch(event);
-  };
-
   return (
     <div>
       <label htmlFor="search">Search: </label>&nbsp;
-      <input id="search" type="text" onChange={handleChange} />
-      <p>
-        Searching for <strong>{searchTerm}</strong>
-      </p>
+      <input id="search" type="text" onChange={onSearch} />
     </div>
   );
 };

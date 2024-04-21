4632ba4a24b94c54ad93063c391e13c46936393d
Astar <bahouidi99@gmail.com>
2024-04-21 20:28:27 +0100

    Handler functions
    In React, the onChange event is commonly used for handling changes in
    input fields, and its behavior is more similar to the native input than
    the native change event in HTML.
    
    If you want behavior similar to the native change event in React, you
    can use the onBlur event. The onBlur event in React is fired when the
    input field loses focus

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,8 +1,13 @@
 const Search = () => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <label htmlFor="search">Search: </label>&nbsp;
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
 };

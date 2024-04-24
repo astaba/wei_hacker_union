65005300aa035faa3f51d3f6529786475ec96f84
Astar <bahouidi99@gmail.com>
2024-04-24 23:30:51 +0100

    Prevent side effect on first render
    There is no React Hook that runs on every re-render, and there is no way
    to tell the `useEffect` hook in a React idiomatic way to call its
    function only on every re-render. However, by using React's `useRef` Hook
    which keeps its `ref.current` property intact over re-renders, we can
    keep a made up state (without re-rendering the component on state updates)
    with an instance variable of our component's lifecycle.

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const useLocalStorage = (
   key: string,
@@ -6,7 +6,14 @@ const useLocalStorage = (
 ): [string, React.Dispatch<React.SetStateAction<string>>] => {
   const [value, setValue] = useState(localStorage.getItem(key) || defaultValue);
 
+  const isMounting = useRef(true);
+
   useEffect(() => {
+    if (isMounting.current) {
+      isMounting.current = false;
+      return;
+    }
+    console.log("A: useLocalStorage: How many times do I run at mounting?");
     localStorage.setItem(key, value);
   }, [key, value]);
 

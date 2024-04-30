53ee2870f1fcbb894dd136e05a00f0c3bb9f84ce
Astar <bahouidi99@gmail.com>
2024-04-30 19:05:05 +0100

    Implement styleX

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { stylexPlugin } from "vite-plugin-stylex-dev";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), stylexPlugin()],
 });
